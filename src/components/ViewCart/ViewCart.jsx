73a654facd1624f827c91be53063562b5d27f131
Alexander Sosov <luisodiz@yandex.ru>
2024-02-05 11:48:41 +0300

    chore

--- src/components/ViewCart/ViewCart.jsx
+++ src/components/ViewCart/ViewCart.jsx
@@ -1,10 +1,25 @@
+import {useRef, useEffect} from 'react'
 import {StyledViewCart} from './StyledViewCart'
 import CartIcon from '../../assets/img/cart-icon.svg?react'
 import {Link} from 'react-router-dom'
 
 const ViewCart = (props) => {
+  const buttonRef = useRef(null)
+
+  const handleClick = () => {
+    if (buttonRef.current) {
+      buttonRef.current.blur()
+    }
+  }
+
   return (
-    <StyledViewCart as={Link} to="/cart" {...props}>
+    <StyledViewCart
+      as={Link}
+      to="/cart"
+      ref={buttonRef}
+      onClick={() => handleClick()}
+      {...props}
+    >
       <CartIcon/>
       View Cart
       <span>22</span>
